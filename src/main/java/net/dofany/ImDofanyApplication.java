e372e499ec42ea7511c633a05d4ef4e8620d24c8
dofany <kimdohwan17@gmail.com>
2019-06-09 16:34:57 +0900

    initial cummit

---
+++ src/main/java/net/dofany/ImDofanyApplication.java
@@ -0,0 +1,13 @@
+package net.dofany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDofanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImDofanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dofany/ImDofanyApplicationTests.java
@@ -0,0 +1,16 @@
+package net.dofany;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImDofanyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
